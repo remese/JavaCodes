21cbe1eb8a783d413174efaafc08b5f78438505e
remese <70172380+remese@users.noreply.github.com>
2020-09-08 20:13:01 +0200

    Update FactorsOfAnInteger.java

--- FactorsOfAnInteger.java
+++ FactorsOfAnInteger.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-
 public class FactorsOfAnInteger {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a positive integer: ");
