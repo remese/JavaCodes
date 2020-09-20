9e746835a20b71b3fc1f573629d90a4980568304
remese <70172380+remese@users.noreply.github.com>
2020-09-20 14:05:38 +0200

    Create Exercise_4_8.java

---
+++ Exercise_4_8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercise_4_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter and ASCII code: ");
+		int DECIMAL = input.nextInt();
+		System.out.println("The character for ASCII code " + DECIMAL + " is " + (char)DECIMAL);
+		//char C = input.next().charAt(0);
+		
+	}
+
+}
