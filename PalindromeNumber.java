f10d315b35d493d92099aadc929e4ad72a593935
remese <70172380+remese@users.noreply.github.com>
2020-09-22 11:31:26 +0200

    Create PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a three-digit integer: ");
+		int number = input.nextInt();
+		if (number / 100 == number % 10) {
+			System.out.println(number + " is a palindrome");
+		} else {
+			System.out.println(number + " is not a palindrome");
+		}
+	}
+
+}
